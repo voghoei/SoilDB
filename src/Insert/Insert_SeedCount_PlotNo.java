1cccac7bc2dce652363a81c44f0bde23a8913e4c
voghoei <voghoei@DESKTOP-8SM3HF1>
2016-12-09 14:05:00 -0500

    Insert seed count

--- src/Insert/Insert_SeedCount_PlotNo.java
+++ src/Insert/Insert_SeedCount_PlotNo.java
@@ -9,9 +9,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -28,44 +25,31 @@ public class Insert_SeedCount_PlotNo {
         String countCurrentLine;
         BufferedReader br ;
         ResultSet rs ;
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-
-
         int counter = 0;
-        int dublication = 0;
+
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433; instance= DESKTOP-8SM3HF1\\SQLEXPRESS;databaseName=Soil;integratedSecurity=true");
             Statement statement = conn.createStatement();
 
-            br = new BufferedReader(new FileReader("E:\\2016 winter data\\Phones\\Sweet_corn_data_Blairsville\\Full\\Total Weight Hi2Lo2 corrected.csv"));
+            br = new BufferedReader(new FileReader("E:\\2016 winter data\\Phones\\Harvest\\2016 NUR Seed Count.csv"));
             br.readLine();
             while ((countCurrentLine = br.readLine()) != null) {
-                String FieldMap_ID = countCurrentLine.split(",")[0];
-                String Total_Wet_Weight = countCurrentLine.split(",")[2];
-                String Sample_Wet_Weight = countCurrentLine.split(",")[3];
-                String Sample_Dry_Weight = countCurrentLine.split(",")[4];
-                String ExcelDate = countCurrentLine.split(",")[5];
+                String PlotNo = countCurrentLine.split(",")[0];
+                String SeedCount = countCurrentLine.split(",")[2];                
+                String q = "select [FieldMap_ID] from [View_FieldMap] where [Destination] like '%NUR%' and [Destination] like '%" + PlotNo + "%' ";
+                rs = statement.executeQuery(q);
+                if (rs.next()) {
+                    String FieldMap_ID = rs.getString(1);                    
                 
-                Date DateTime = formatter.parse(ExcelDate);
-                java.sql.Timestamp sqlDateTime = new java.sql.Timestamp(DateTime.getTime());
-
-                String Query = "insert into FieldBook(FieldMap_ID,Phenotype_ID,Value,Date) values (" + FieldMap_ID + ",13, '" + Total_Wet_Weight + "','" + sqlDateTime + "')";
-                statement.executeUpdate(Query);
-                
-                Query = "insert into FieldBook(FieldMap_ID,Phenotype_ID,Value,Date) values (" + FieldMap_ID + ",14, '" + Sample_Wet_Weight + "','" + sqlDateTime + "')";
-                statement.executeUpdate(Query);
-                
-                Query = "insert into FieldBook(FieldMap_ID,Phenotype_ID,Value,Date) values (" + FieldMap_ID + ",15, '" + Sample_Dry_Weight + "','" + sqlDateTime + "')";
-                statement.executeUpdate(Query);
-                
+                String Query = "insert into FieldBook(FieldMap_ID,Phenotype_ID,Value,Date) values (" + FieldMap_ID + ",9, '" + SeedCount + "','2016-11-15 10:00:00.000')";
+                statement.executeUpdate(Query); 
                 counter++;
-                
+               } 
             }
             System.out.println(counter + " Rows Data Inserted");
-            System.out.println(dublication + " Rows Data Dublicated");
 
-        } catch (ClassNotFoundException | SQLException | IOException | ParseException e) {
+        } catch (ClassNotFoundException | SQLException | IOException e) {
             System.err.println("Problem Connecting!  " + e);
         }
 

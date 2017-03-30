fb18344ccc84fb9d344c068e5e25f55f648dea82
voghoei <voghoei@DESKTOP-8SM3HF1>
2017-03-30 14:34:37 -0400

    insert Experiment and Withdrawal

--- src/Insert/InsertExperiment.java
+++ src/Insert/InsertExperiment.java
@@ -19,78 +19,88 @@ public class InsertExperiment {
             Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433; instance= DESKTOP-8SM3HF1\\SQLEXPRESS;databaseName=Soil;integratedSecurity=true");
             Statement statement = conn.createStatement();
 
-            br = new BufferedReader(new FileReader("E:\\2016 winter data\\Soil\\2017\\Package info\\Plains.csv"));
+            br = new BufferedReader(new FileReader("E:\\2016 winter data\\Soil\\2017\\Package info\\SeedTreatments.csv"));
             int counter = 0;
             br.readLine();
             while ((countCurrentLine = br.readLine()) != null) {
 
                 String Name = countCurrentLine.split(",")[10].split("_")[0];
                 String Treatment = countCurrentLine.split(",")[10].split("_")[1];
-                String FileOrigin = countCurrentLine.split(",")[0].replaceAll(" ", "");
+                String FileOrigin = "Patric Ag Chemical";
                 String Destination = countCurrentLine.split(",")[9].replaceAll(" ", "");
-                String Block = Destination.split("-")[2];
+                String Rep = Destination.split("-")[2];
                 String Plot = Destination.split("-")[3];
-                
+
                 String Origin = Destination.split("-")[0].substring(2, 4);
                 String ExperimentType = Destination.split("-")[1];
                 int SeedNo = 100; //countCurrentLine.split(",")[11];
-                String Note = countCurrentLine.split(",")[13];
-                
-                Note = Note + " - "+ Treatment;
+                String Note = countCurrentLine.split(",")[13];               
 
                 int ExperimentType_ID = 0;
-                int Block_ID = 0;
+                int Rep_ID = 0;
                 int Origin_ID = 0;
                 int Experiment_ID = 0;
                 int Plot_ID = 0;
                 int Package_ID = 0;
+                int Treatment_ID = 0;
 
+                // Origin
                 rs = statement.executeQuery("select ID from Origin where code = '" + Origin + "'");
                 if (rs.next()) {
                     Origin_ID = rs.getInt(1);
+                    //ExperimentType
                     rs = statement.executeQuery("select ID from ExperimentType where code = '" + ExperimentType + "'");
-                    
                     if (rs.next()) {
                         ExperimentType_ID = rs.getInt(1);
+                        //Plot
                         rs = statement.executeQuery("select ID from Plot where Plot = '" + Plot + "'");
-                        
                         if (rs.next()) {
                             Plot_ID = rs.getInt(1);
-                            rs = statement.executeQuery("select ID from Block where code = '" + Block + "'");
+                            //Treatment
+                            rs = statement.executeQuery("select ID from Treatment where Name = '" + Treatment + "'");
                             if (rs.next()) {
-                                Block_ID = rs.getInt(1);
-                                statement.executeUpdate("INSERT INTO [dbo].[Experiment1] ([ExperimentType_ID],[location_ID],[Block_ID],[Plot_ID],[Year],[Note]) VALUES(" + ExperimentType_ID + "," + Origin_ID + "," + Block_ID + ",'" + Plot_ID + "',17,'"+Note+"')");
-                            } else {
-                                statement.executeUpdate("INSERT INTO [dbo].[Experiment1] ([ExperimentType_ID],[location_ID],[Block_ID],[Plot_ID],[Year],[Note]) VALUES(" + ExperimentType_ID + "," + Origin_ID + ",Null,'" + Plot_ID + "',17,'"+Note+"')");
-                            }
-                            counter++;
-                            rs = statement.executeQuery("select IDENT_CURRENT('Experiment1')");
-                            if (rs.next()) {
-                                Experiment_ID = rs.getInt(1);
-                                String q = "select ID from View_Package1 where replace(Name,' ','') = '" + Name + "' and replace(Origin,' ','') = '" + FileOrigin + "'";
-//                            String q ="select ID from View_Package where replace(Name,' ','') = '" + Name + "' and replace(Origin,' ','') = '" + FileOrigin + "' and Date_In = '2016-04-01'";
-                                rs = statement.executeQuery(q);
+                                Treatment_ID = rs.getInt(1);
+                                //Package
+                                rs = statement.executeQuery("select ID from View_Package where Name = '" + Name + "' and Origin = '" + FileOrigin + "'");
                                 if (rs.next()) {
                                     Package_ID = rs.getInt(1);
-//                                if (Note.length() > 1){    
-//                                   // String Q = "INSERT INTO [dbo].[PackageWithdrawal1] ([Package_ID],[Out_ID],[Experiment_ID],[Person_ID],[Date],[SeedRemove],[Note]) VALUES(" + Package_ID + ",null," + Experiment_ID + ",1,CONVERT (date, GETDATE())," + SeedNo + "," + Note + ")";
-//                                    statement.executeUpdate("INSERT INTO [dbo].[PackageWithdrawal1] ([Package_ID],[Out_ID],[Experiment_ID],[Person_ID],[Date],[SeedRemove],[Note]) VALUES(" + Package_ID + ",null," + Experiment_ID + ",1,CONVERT (date, GETDATE())," + SeedNo + ",'" + Note + "')");
-//
-//                                } else {
-                                    statement.executeUpdate("INSERT INTO [dbo].[PackageWithdrawal1] ([Package_ID],[Out_ID],[Experiment_ID],[Person_ID],[Date],[SeedRemove],[Note]) VALUES(" + Package_ID + ",null," + Experiment_ID + ",1,CONVERT (date, GETDATE())," + SeedNo + ",null)");
+                                    //Rep
+                                    rs = statement.executeQuery("select ID from Rep where code = '" + Rep + "'");
+                                    //Insert Experiment
+                                    if (rs.next()) {
+                                        Rep_ID = rs.getInt(1);
+                                        statement.executeUpdate("INSERT INTO [dbo].[Experiment1] ([ExperimentType_ID],[location_ID],[Rep_ID],[Plot_ID],[Year],[Note],Treatment_ID) VALUES(" + ExperimentType_ID + "," + Origin_ID + "," + Rep_ID + ",'" + Plot_ID + "',17,'" + Note + "',"+Treatment_ID+")");
+                                    } else {
+                                        statement.executeUpdate("INSERT INTO [dbo].[Experiment1] ([ExperimentType_ID],[location_ID],[Rep_ID],[Plot_ID],[Year],[Note],Treatment_ID) VALUES(" + ExperimentType_ID + "," + Origin_ID + ",Null,'" + Plot_ID + "',17,'" + Note + "',"+Treatment_ID+")");
+                                    }
+                                    counter++;
+                                    //Experiment
+                                    rs = statement.executeQuery("select IDENT_CURRENT('Experiment1')");
+                                    if (rs.next()) {
+                                        Experiment_ID = rs.getInt(1);
+                                        //Package
 
-//                                }
+                                        //if (Note.length() > 1){    
+                                        //    statement.executeUpdate("INSERT INTO [dbo].[PackageWithdrawal1] ([Package_ID],[Out_ID],[Experiment_ID],[Person_ID],[Date],[SeedRemove],[Note]) VALUES(" + Package_ID + ",null," + Experiment_ID + ",1,CONVERT (date, GETDATE())," + SeedNo + ",'" + Note + "')");//
+                                        //} else {
+                                        //Insert PackageWithdrawal
+                                        statement.executeUpdate("INSERT INTO [dbo].[PackageWithdrawal1] ([Package_ID],[Out_ID],[Experiment_ID],[Person_ID],[Date],[SeedRemove],[Note]) VALUES(" + Package_ID + ",null," + Experiment_ID + ",1,CONVERT (date, GETDATE())," + SeedNo + ",null)");
+
+                                        // }
+                                    } else {
+                                        System.out.println("There is no record Inserted recently");
+                                    }
+
                                 } else {
                                     System.out.println("There is no record Package with" + Name + " and " + FileOrigin);
                                 }
-
                             } else {
-                                System.out.println("There is no Experiment_ID");
+                                System.out.println("There is no record Treatment with " + Treatment);
+
                             }
                         } else {
                             System.out.println("There is no record with " + Plot);
                         }
-
                     } else {
                         System.out.println("There is no record with " + ExperimentType);
                     }
